0e656d79c24f04ba461ddd05b94fb4958d50fd04
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2022-11-12 13:38:58 -0300

    feat: controller, input type and ESLINT config

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { CidadeController } from './../controllers';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.status(StatusCodes.OK).send('Olá, DEV!');
+  return res.status(StatusCodes.OK).send('Funcionando!');
 });
+
+router.post('/cidades', CidadeController.create);
 
 export { router };
